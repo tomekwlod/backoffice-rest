a6f79be0f78faf3dae1cffe333ae730100005017
Tomasz Wlodarczyk <twl@phase-ii.com>
2018-05-23 10:10:53 +0100

    Start a server script

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+list="$(ps aux | grep os-utils | grep -v grep)"
+pids="$(echo -e "${list}" | awk '{print $2}')"
+
+#if ((${#pids[@]} > 0)); then
+    echo -e "\nRunning go servers:\n > ${list}\n"
+    echo -e "\nBelow PIDs will be killed:\n > ${pids}\n"
+
+    for pid in ${pids}
+    do
+        echo " -> killing $pid"
+        kill -s 9 ${pid} && echo '    killed' || echo "     couldn't kill"
+    done
+#fi
+
+echo -e "\nStaring a new server... "
+
+./os-utils >> /var/go/os-tools/logs/httpd.log 2>&1 &
+
+echo -e "Done"
+
+exit 0;
